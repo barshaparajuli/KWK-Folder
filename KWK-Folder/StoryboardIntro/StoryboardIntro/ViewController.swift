bf25771d4a8234605d1d457aa83f1e2580e01c30
barshaparajuli <40861964+barshaparajuli@users.noreply.github.com>
2021-07-14 11:38:55 -0400

    Add files via upload

---
+++ KWK-Folder/StoryboardIntro/StoryboardIntro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardIntro
+//
+//  Created by Barsha Parajuli on 7/14/21.
+//  Copyright © 2021 Barsha Parajuli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
